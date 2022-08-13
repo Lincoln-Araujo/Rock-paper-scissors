a4c0c3e9333c3f44f72b0ace52865f180e9f61dc
Lincoln Araujo <72479081+Lincoln-Araujo@users.noreply.github.com>
2022-08-13 16:34:06 -0300

    fixing code
    fixing bugs in the code

---
+++ rockPaperScissors.js
@@ -0,0 +1,110 @@
+// variables
+let scorePlayer = 0;
+
+let scoreComputer = 0;
+
+let round = 1;
+
+let result = '';
+
+let options = ['rock', 'paper', 'scissors'];
+    
+// functions
+
+// computer choice
+function computerPlay() {
+
+    let sortedOption = Math.floor(Math.random() * options.length);
+
+    return options[sortedOption];
+
+}
+
+// playing a round
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection == "paper" && computerSelection == "scissors") {
+        scoreComputer++;
+        return "You Lose! Scissors cuts paper."
+    } else if ( playerSelection == "scissors" && computerSelection == "rock") {
+        scoreComputer++;
+        return "You Lose! Rock breaks scissors."
+    } else if ( playerSelection == "rock" && computerSelection == "paper") {
+        scoreComputer++;
+        return "You Lose! Paper covers rock."
+    } else if ( playerSelection == "paper" && computerSelection == "rock") {
+        scorePlayer++;
+        return "You Win! Paper covers rock."
+    } else if ( playerSelection == "scissors" && computerSelection == "paper") {
+        scorePlayer++;
+        return "You Win! Scissors cuts paper."
+    } else if ( playerSelection == "rock" && computerSelection == "scissors") {
+        scorePlayer++;
+        return "You Win! Rock breaks scissors."
+    } else if ( playerSelection == computerSelection) {
+        return "Oh, it's a draw!"
+    } else {
+        return "You can only choose paper, scissors or rock"
+    }
+        
+}
+
+// main function game
+function game() {    
+
+    let playerChoice =  prompt(`Round ${round} - Choose rock, paper or scissors`);
+
+    // preventing user's input error
+    playerChoice = playerChoice.toLowerCase();
+
+    let computerSelection = computerPlay(); 
+
+    if (options.includes(playerChoice)) {
+        
+        while(scorePlayer < 5 || scoreComputer < 5) {
+            console.log(`------------ROUND ${round}------------`);
+            console.log(`YOU: ${playerChoice} - COMPUTER: ${computerSelection}`);
+            console.log(playRound(playerChoice, computerSelection));
+            console.log(`------ You: ${scorePlayer} X Computer: ${scoreComputer} ------`);
+            console.log(" ");
+            round++;
+            if (scorePlayer >= 5 && scorePlayer > scoreComputer) {
+                alert('You are the winner!');
+                round = 0;
+                scorePlayer = 0;
+                scoreComputer = 0;
+            } else if (scoreComputer >= 5 && scorePlayer < scoreComputer) {
+                alert('Sorry, computer is the winner!');
+                round = 0;
+                scorePlayer = 0;
+                scoreComputer = 0; 
+            } else if (scoreComputer == 5 && scorePlayer == scoreComputer) {
+                alert('Oh, it is a draw!');
+                round = 0;
+                scorePlayer = 0;
+                scoreComputer = 0;
+            } else {
+                game();
+            }
+            
+            break;
+        }       
+
+    } else {
+        console.log("please, choose rock, paper or scissors");
+        game()
+    }
+    
+    switch(scorePlayer || scoreComputer) {
+
+        case (5):
+            
+            break
+        default:
+            
+
+    }    
+    
+}
+
+console.log(game());
