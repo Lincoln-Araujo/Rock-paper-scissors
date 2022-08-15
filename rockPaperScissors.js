a727be5cacba4d80987f5cdb0ba75bb2eb03a336
Lincoln Araujo <72479081+Lincoln-Araujo@users.noreply.github.com>
2022-08-15 09:17:23 -0300

    Adding welcome and orientation messages

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -5,8 +5,6 @@ let scoreComputer = 0;
 
 let round = 1;
 
-let result = '';
-
 let options = ['rock', 'paper', 'scissors'];
     
 // functions
@@ -24,27 +22,49 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection == "paper" && computerSelection == "scissors") {
+
         scoreComputer++;
-        return "You Lose! Scissors cuts paper."
+
+        return "You Lose! Scissors cuts paper.";
+
     } else if ( playerSelection == "scissors" && computerSelection == "rock") {
+
         scoreComputer++;
-        return "You Lose! Rock breaks scissors."
+
+        return "You Lose! Rock breaks scissors.";
+
     } else if ( playerSelection == "rock" && computerSelection == "paper") {
+
         scoreComputer++;
-        return "You Lose! Paper covers rock."
+
+        return "You Lose! Paper covers rock.";
+
     } else if ( playerSelection == "paper" && computerSelection == "rock") {
+
         scorePlayer++;
-        return "You Win! Paper covers rock."
+
+        return "You Win! Paper covers rock.";
+
     } else if ( playerSelection == "scissors" && computerSelection == "paper") {
+
         scorePlayer++;
-        return "You Win! Scissors cuts paper."
+
+        return "You Win! Scissors cuts paper.";
+
     } else if ( playerSelection == "rock" && computerSelection == "scissors") {
+
         scorePlayer++;
-        return "You Win! Rock breaks scissors."
+
+        return "You Win! Rock breaks scissors.";
+
     } else if ( playerSelection == computerSelection) {
-        return "Oh, it's a draw!"
+
+        return "Oh, it's a draw!";
+
     } else {
-        return "You can only choose paper, scissors or rock"
+
+        return "You can only choose paper, scissors or rock";
+
     }
         
 }
@@ -62,49 +82,53 @@ function game() {
     if (options.includes(playerChoice)) {
         
         while(scorePlayer < 5 || scoreComputer < 5) {
+
             console.log(`------------ROUND ${round}------------`);
+
             console.log(`YOU: ${playerChoice} - COMPUTER: ${computerSelection}`);
+
             console.log(playRound(playerChoice, computerSelection));
+
             console.log(`------ You: ${scorePlayer} X Computer: ${scoreComputer} ------`);
+
             console.log(" ");
+
             round++;
+
             if (scorePlayer >= 5 && scorePlayer > scoreComputer) {
+
                 alert('You are the winner!');
-                round = 0;
-                scorePlayer = 0;
-                scoreComputer = 0;
+
             } else if (scoreComputer >= 5 && scorePlayer < scoreComputer) {
+
                 alert('Sorry, computer is the winner!');
-                round = 0;
-                scorePlayer = 0;
-                scoreComputer = 0; 
+
             } else if (scoreComputer == 5 && scorePlayer == scoreComputer) {
+
                 alert('Oh, it is a draw!');
-                round = 0;
-                scorePlayer = 0;
-                scoreComputer = 0;
+
             } else {
+
                 game();
+
             }
             
             break;
+
         }       
 
     } else {
+
         console.log("please, choose rock, paper or scissors");
-        game()
+
+        game();
+
     }
-    
-    switch(scorePlayer || scoreComputer) {
-
-        case (5):
-            
-            break
-        default:
-            
-
-    }    
     
 }
 
+console.log("Welcome to Rock, Paper & Scissors game");
+console.log("Win 5 rounds to be THE CHAMPION");
+alert("Welcome to Rock, Paper & Scissors game, Win 5 rounds to be THE CHAMPION");
+alert("Oh, open your browser console to check the results");
 console.log(game());
